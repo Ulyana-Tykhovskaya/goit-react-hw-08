65aa232b664558ee9fa81fef8e0946669c8d16ab
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-05-13 15:49:52 +0200

    update-page

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import Filter from "../../components/Filter/Filter";
 import ContactList from "../../components/ContactList/ContactList";
 
 export default function ContactsPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div style={{ padding: 20 }}>
       <h2>Contacts</h2>
